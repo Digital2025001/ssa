29a0d88b3229ee9a02987dffc02370b56bc4f5d9
Digital2025001 <bosmichael30@gmail.com>
2025-09-27 11:29:03 -0500

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,10 @@
+import { serve } from "https://deno.land/std/http/server.ts";
+
+serve((_req) => {
+  return new Response(null, {
+    status: 302,
+    headers: {
+      Location: "https://betterwebplace.com/", // Replace this with your actual link
+    },
+  });
+});
